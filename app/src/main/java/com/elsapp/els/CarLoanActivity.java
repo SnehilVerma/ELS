a1682a9f9845243e57a6ccfa5b96ebf69e6bebb4
SnehilVerma <romilvrm@gmail.com>
2017-06-09 16:29:11 +0530

    slider3

--- app/src/main/java/com/elsapp/els/CarLoanActivity.java
+++ app/src/main/java/com/elsapp/els/CarLoanActivity.java
@@ -1,5 +1,6 @@
 package com.elsapp.els;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,6 +8,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,7 @@ public class CarLoanActivity extends AppCompatActivity {
 
 
     private ViewPager viewPager;
+    int x=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +33,14 @@ public class CarLoanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_car_loan);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        TextView tv=(TextView)findViewById(R.id.tv1);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/roboto.ttf");
+        tv.setTypeface(font);
         viewPager=(ViewPager)findViewById(R.id.viewpager);
         viewPager.setPageTransformer(true,new PagerTransformer());
+
+
+
         setUpViewPager(viewPager);
 
     }
@@ -44,8 +53,7 @@ public class CarLoanActivity extends AppCompatActivity {
         adapter.addFragment(new PrefCar(), "PrefCar");
         adapter.addFragment(new Gender(),"Gender");
         adapter.addFragment(new Retired_P(), "Retired_Pensioner");
-        adapter.addFragment(new Self_Employed(), "Self_Employed" +
-                "");
+        adapter.addFragment(new Self_Employed(), "Self_Employed");
 
         viewPager.setAdapter(adapter);
     }
@@ -61,6 +69,12 @@ public class CarLoanActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
+            //MODIFY THE LOGIC
+            /*
+            if(position==2){
+            Fragment f=new Retired_P();
+            return f;}*/
+
             return mFragmentList.get(position);
         }
 
