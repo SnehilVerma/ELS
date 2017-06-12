803069ceec575d6dd43e9c4c93b9b88a766a3216
SnehilVerma <romilvrm@gmail.com>
2017-06-12 18:02:18 +0530

    gg

--- app/src/main/java/Fragments/Select_Category.java
+++ app/src/main/java/Fragments/Select_Category.java
@@ -1,6 +1,5 @@
 package Fragments;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,24 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.elsapp.els.R;
-
 /**
- * Created by snehil on 9/6/17.
+ * Created by snehil on 12/6/17.
  */
 
-public class Gender extends Fragment {
+public class Select_Category extends Fragment {
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-
-        final View view=inflater.inflate(R.layout.fragment_gender, container, false);
-
-
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view=inflater.inflate(R.layout.fragment_select_cat, container, false);
 
 
         return view;
-
     }
-
-
 }
