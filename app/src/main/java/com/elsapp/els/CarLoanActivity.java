86f562529c13ecc67b2a78c9a2639525f2928d1f
sibby97 <sid.thecooljain@gmail.com>
2017-06-15 18:21:27 +0530

    babes

--- app/src/main/java/com/elsapp/els/CarLoanActivity.java
+++ app/src/main/java/com/elsapp/els/CarLoanActivity.java
@@ -138,6 +138,7 @@ public class CarLoanActivity extends AppCompatActivity
             Intent intent = new Intent(CarLoanActivity.this,LoanSelec.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
+
         }
 
 
