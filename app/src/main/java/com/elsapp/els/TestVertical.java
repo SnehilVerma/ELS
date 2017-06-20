ab11fa46bc21a60ae84b5bc5c6c78d283e038f19
SnehilVerma <romilvrm@gmail.com>
2017-06-20 15:50:11 +0530

    Vertical View Pager added

---
+++ app/src/main/java/com/elsapp/els/TestVertical.java
@@ -0,0 +1,87 @@
+package com.elsapp.els;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Fragments.LoanRepayment;
+import Fragments.LoanSpecific;
+import Fragments.Personal;
+import Utility.VerticalViewPager;
+
+/**
+ * Created by snehil on 20/6/17.
+ */
+
+public class TestVertical extends BaseActivity {
+
+
+    VerticalViewPager verticalViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_vertical);
+
+        verticalViewPager=(VerticalViewPager)findViewById(R.id.viewpager);
+
+        setupViewPager(verticalViewPager);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        TestVertical.ViewPagerAdapter adapter=new TestVertical.ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new Personal(), "Personal");
+        adapter.addFragment(new LoanSpecific(), "LoanSpecific");
+        adapter.addFragment(new LoanRepayment(), "LoanRepayment");
+        viewPager.setAdapter(adapter);
+
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            return mFragmentTitleList.get(position);
+
+        }
+    }
+}
