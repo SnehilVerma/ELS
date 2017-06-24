23ee3271fae2febb5cf086cfeb1ec2cab233e77c
DrunkenMaestro <bphani1996@gmail.com>
2017-06-24 13:54:06 +0530

    hie

---
+++ app/src/main/java/Fragments/LandPurchase1.java
@@ -0,0 +1,25 @@
+package Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.elsapp.els.R;
+
+
+
+public class LandPurchase1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View x = inflater.inflate(R.layout.fragment_land_purchase1, container, false);
+        return x;
+    }
+}
